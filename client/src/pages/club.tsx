82e5c7f02aea65831ea3f5605c7c263df247dc86
connorwmackay <connormackay76@gmail.com>
2021-09-21 19:57:22 +0100

    Added Club nav menu

--- client/src/pages/club.tsx
+++ client/src/pages/club.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAuthenticatedUser, selectAuthenticatedUser } from '../features/authenticatedUser';
 import { useParams } from 'react-router';
 
 import '../styles/User.css'
+import '../styles/Club.css'
 import { selectFindClub, fetchClub, resetClubState } from '../features/findClub';
 import { selectJoinClub, fetchJoinClub } from '../features/joinClub';
 import { selectFindClubMember, fetchFindClubMember } from '../features/findClubMember';
@@ -12,6 +13,12 @@ import { selectFindAllClubMembers, fetchAllClubMembers } from '../features/findA
 interface ClubParams {
     uuid: string;
 }
+
+enum ClubNavMenuItem {
+    BULLETIN,
+    CHAT,
+    MEMBERS
+};
 
 const Club = () => {
     const authenticatedUser = useSelector(selectAuthenticatedUser);
@@ -23,6 +30,8 @@ const Club = () => {
     const dispatch = useDispatch();
 
     const params: ClubParams = useParams();
+
+    const [currentMenuItem, setCurrentMenuItem] = useState(ClubNavMenuItem.MEMBERS);
 
     useEffect(() => {
         if (!findClub.is_club_found) {
@@ -61,11 +70,93 @@ const Club = () => {
             return <li key={member.id}>
                 <a href={`http://localhost:3000/user/${member.user.username}`}>
                     <img src={`http://localhost:4001${member.user.profile_picture}`} alt="Profile"
-                    width="25" height="25" />
-                    {member.user.username}
+                    width="35" height="35" /> {member.user.username}
                 </a>
             </li>
         });
+
+        const clubNavMenu = () => {
+
+            let bulletinClasses = "club-nav-button";
+            let chatClasses = "club-nav-button";
+            let membersClasses = "club-nav-button" 
+
+            switch(currentMenuItem) {
+                case ClubNavMenuItem.BULLETIN:
+                    bulletinClasses = "club-nav-button-current";
+                    chatClasses = "club-nav-button";
+                    membersClasses = "club-nav-button";
+                    break;
+                case ClubNavMenuItem.CHAT:
+                    bulletinClasses = "club-nav-button";
+                    chatClasses = "club-nav-button-current";
+                    membersClasses = "club-nav-button";
+                    break;
+                case ClubNavMenuItem.MEMBERS:
+                    bulletinClasses = "club-nav-button";
+                    chatClasses = "club-nav-button";
+                    membersClasses = "club-nav-button-current";
+                    break;
+            }
+
+            return (
+                <nav className="club-nav">
+                    <ul className="club-nav-list">
+                        <li className="club-nav-item">
+                            <button className={bulletinClasses}>Bulletin</button>
+                        </li>
+                        <li className="club-nav-item">
+                            <button className={chatClasses}>Chat</button>
+                        </li>
+                        <li className="club-nav-item">
+                            <button className={membersClasses}>Members</button>
+                        </li>
+                    </ul>
+                </nav>
+            );
+        };
+
+        const clubMemberList = () => {
+            return (
+                <ul className="member-list">
+                    <li key={findClub.owner.id}>
+                        <a href={`http://localhost:3000/user/${findClub.owner.username}`}>
+                            <img src={`http://localhost:4001${findClub.owner.profile_picture}`} alt="Profile"
+                            width="35" height="35" /> {findClub.owner.username}
+                        </a>
+                    </li>
+
+                    {memberListItems}
+                </ul>
+            )
+        };
+
+        const clubContent = () => {
+            switch(currentMenuItem) {
+                case ClubNavMenuItem.BULLETIN:
+                    return (
+                        <div>
+                        {clubNavMenu()}
+                         <h2 className="club-sub-header">Bulletin</h2>
+                    </div>
+                    );
+                case ClubNavMenuItem.CHAT:
+                    return (
+                        <div>
+                            {clubNavMenu()}
+                             <h2 className="club-sub-header">Chat</h2>
+                        </div>
+                    );
+                case ClubNavMenuItem.MEMBERS:
+                    return (
+                        <div>
+                            {clubNavMenu()}
+                             <h2 className="club-sub-header">Members</h2>
+                            {clubMemberList()}
+                        </div>
+                    );
+            }
+        }
 
         if (findClub.owner.username === authenticatedUser.username) { // TODO: Add Cover photo
             return (
@@ -76,9 +167,9 @@ const Club = () => {
                         </div>
                         <h1>{findClub.name}</h1>
                         <p>{findClub.description}</p>
-                        <ul>
-                            {memberListItems}
-                        </ul>
+                        
+                        {clubContent()}
+                        
                     </section>
                 </div>
             );
@@ -94,17 +185,8 @@ const Club = () => {
                         
                         <h1>{findClub.name}</h1>
                         <p>{findClub.description}</p>
-                        <ul>
-                            <li key={findClub.owner.id}>
-                                <a href={`http://localhost:3000/user/${findClub.owner.username}`}>
-                                    <img src={`http://localhost:4001${findClub.owner.profile_picture}`} alt="Profile"
-                                    width="25" height="25" />
-                                    {findClub.owner.username}
-                                </a>
-                            </li>
 
-                            {memberListItems}
-                        </ul>
+                        {clubContent()}
                     </section>
                 </div>
             )
@@ -122,10 +204,6 @@ const Club = () => {
                         <form onSubmit={joinClubFunc}>
                             <button type="submit">{joinClubText}</button>
                         </form>
-
-                        <ul>
-                            {memberListItems}
-                        </ul>
                     </section>
                 </div>
             );
